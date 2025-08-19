6994c514c1ea8310b35be6841a8cca59166dc4d9
40325013 <lmcdowell27@qub.ac.uk>
2025-08-19 13:38:59 +0100

    Push

--- test_main.py
+++ test_main.py
@@ -3,6 +3,6 @@ from main import app
 
 def test_health():
     client = TestClient(app)
-    r = client.get("/health")
+    r = client.get("/health-check")
     assert r.status_code == 200
     assert r.json().get("status") == "ok"
