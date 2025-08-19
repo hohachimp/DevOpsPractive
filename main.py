a841aec068003a336ae4446fd41b2a208669374a
40325013 <lmcdowell27@qub.ac.uk>
2025-08-19 13:29:30 +0100

    Push

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
 from fastapi import FastAPI
+
 app = FastAPI()
+MESSAGE = os.getenv("APP_MESSAGE", "Hello from Lucas!")
 
 @app.get("/")
 def home():
-    return {"message": "Hello from Lucas!"}
+    return {"message": MESSAGE}
+
+@app.get("/health-check")
+def health():
+    return {"status": "ok"}
